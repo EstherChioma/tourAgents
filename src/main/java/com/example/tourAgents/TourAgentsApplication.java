708686d830fe3e5e39aaa0e4d340e0b27a1852ef
NnalecheEsther <estherchioma@gmail.com>
2021-02-10 13:38:57 +0100

    A tour booking application

---
+++ src/main/java/com/example/tourAgents/TourAgentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.tourAgents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourAgentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourAgentsApplication.class, args);
+	}
+
+}
